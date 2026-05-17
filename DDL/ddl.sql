05cf70e1fef2be6b4017b0bbd3e4222c1f89af7b
RaihanK <raihank0192@gmail.com>
2026-05-17 20:40:41 +0600

    DDL basic started

---
+++ DDL/ddl.sql
@@ -0,0 +1,25 @@
+-- Create table with constraints
+
+create table persons (
+	id int not null,
+	person_name varchar(50) not null,
+	birth_date Date,
+	phone varchar(15) not null
+	constraint pk_person PRIMARY KEY (id)
+);
+
+-- Add a new column to the existing table
+alter table persons add email varchar(50) not null;
+
+-- Add static value and the number 
+
+select 123 as Static_number ;
+
+select 'Hellow' as Static_value;
+
+select 
+	id,
+	first_name,
+	'New_Customer' as Customer_type
+from customers;
+
