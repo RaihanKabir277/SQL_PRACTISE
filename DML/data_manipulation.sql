7b62ec679da789b6fa681829ebeb53a7b21b4ab8
RaihanK <raihank0192@gmail.com>
2026-05-17 21:41:55 +0600

    update the column with new values

--- DML/data_manipulation.sql
+++ DML/data_manipulation.sql
@@ -2,3 +2,26 @@
 Insert into customers values
                          (6,'anna', 'USA', NULL),
                          (7,'sam', NULL,100)
+
+-- moving data from one table to another
+insert into persons
+
+select 
+	id ,
+	first_name,
+	null,
+	'unknown'
+from customers
+
+-- update the table with new values
+
+update customers
+set score = 0,
+    country = 'UK'
+where id = 6   -- if we does not specify the where clause then all the records will be updated with 0
+
+-- anothr type where replace all null score with 0
+update customers 
+set score = 0
+where score is null
+
