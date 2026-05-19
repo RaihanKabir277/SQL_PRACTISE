42f33474770ed53babc0454061fae54cad78969a
RaihanK <raihank0192@gmail.com>
2026-05-19 23:03:14 +0600

    filtering the data with where clause

---
+++ FILTERING/filtering_data.sql
@@ -0,0 +1,10 @@
+
+select * from customers
+where country = 'USA'  -- filter the data based on country
+
+select * from customers
+where score > 80   -- filter the data based on score
+
+select * from customers
+where country = 'USA' and score > 80   -- filter the data based on country and score
+
