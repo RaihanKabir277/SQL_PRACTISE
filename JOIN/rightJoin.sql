83490c49c9bfbee46d2b73bfc06b445b36431594
RaihanKabir277 <raihank0192@gmail.com>
2026-06-08 14:25:02 +0600

    right join added here

---
+++ JOIN/rightJoin.sql
@@ -0,0 +1,9 @@
+select 
+b.brand_code,
+b.brand_name,
+b.category_code,
+c.category_name 
+from ecommerce.bronze.bronz_brands as b 
+right join ecommerce.bronze.bronz_category as c 
+on upper(b.category_code) = upper(c.category_code)
+
